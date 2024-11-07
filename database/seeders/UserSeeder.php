1808ab7ce11aaeb41b4cffb572d0acfebda7e156
Joshua Pacho <102531192+joshdevkit@users.noreply.github.com>
2024-11-07 11:30:31 +0800

    added default user account for admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,24 +5,22 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
         $role = Role::firstOrCreate(['name' => 'Admin']);
 
         $user = User::create([
             'name' => 'Admin User',
-            'email' => 'admin@example.com',
-            'password' => bcrypt('password'), // Use a secure password
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('12345678'),
         ]);
 
-        // Assign the Admin role to the user
         $user->assignRole($role);
     }
 }
